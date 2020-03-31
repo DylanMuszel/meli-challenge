3fae6f33b672b1f3e14532a97a338efce3d89d7c
Dylan Muszel <dylan.muszel@wolox.com.ar>
2020-03-31 03:49:58 -0300

    Fixing

--- app/src/androidTest/java/com/dylanmuszel/melichallenge/presentation/search/SearchFragmentTest.kt
+++ app/src/androidTest/java/com/dylanmuszel/melichallenge/presentation/search/SearchFragmentTest.kt
@@ -20,7 +20,7 @@ class SearchFragmentTest {
     }
 
     @Test
-    fun whenShowSearchEmptyErrorThenSearchInputHasErrora() {
+    fun whenShowSearchEmptyErrorThenSearchInputHasError() {
 
         // GIVEN
 
