b5648df5756058f4586930c2c9d8aca2f98155f8
Dylan Muszel <dylan.muszel@wolox.com.ar>
2020-03-31 02:11:26 -0300

    Adding use case and source tests

--- app/src/main/java/com/dylanmuszel/melichallenge/framework/product/ProductNetworkDataSource.kt
+++ app/src/main/java/com/dylanmuszel/melichallenge/framework/product/ProductNetworkDataSource.kt
@@ -2,8 +2,8 @@ package com.dylanmuszel.melichallenge.framework.product
 
 import arrow.core.Either
 import com.dylanmuszel.core.fp.Failure
-import com.dylanmuszel.core.fp.NetworkConnection
-import com.dylanmuszel.core.fp.ServerError
+import com.dylanmuszel.core.fp.NetworkConnectionFailure
+import com.dylanmuszel.core.fp.ServerFailure
 import com.dylanmuszel.data.ProductDataSource
 import com.dylanmuszel.domain.Product
 import com.dylanmuszel.melichallenge.framework.core.network.ConnectivityInfo
@@ -20,19 +20,19 @@ class ProductNetworkDataSource(
     /**
      * Search products by a query.
      *
-     * If the device is offline, it'll return a [NetworkConnection]
-     * If the server throws an unexpected error, it'll return a [ServerError]
+     * If the device is offline, it'll return a [NetworkConnectionFailure]
+     * If the server throws an unexpected error, it'll return a [ServerFailure]
      */
     override suspend fun search(query: String): Either<Failure, List<Product>> {
 
         if (!connectivityInfo.isOnline) {
-            return Either.Left(NetworkConnection)
+            return Either.Left(NetworkConnectionFailure)
         }
 
         return try {
             Either.Right(productService.search(query).results)
         } catch (exception: HttpException) {
-            Either.Left(ServerError(exception))
+            Either.Left(ServerFailure(exception))
         }
     }
 }
---
+++ app/src/test/java/com/dylanmuszel/melichallenge/framework/product/ProductNetworkDataSourceTest.kt
@@ -0,0 +1,83 @@
+package com.dylanmuszel.melichallenge.framework.product
+
+import android.net.Network
+import arrow.core.Either
+import arrow.core.orNull
+import com.dylanmuszel.core.fp.NetworkConnectionFailure
+import com.dylanmuszel.core.fp.ServerFailure
+import com.dylanmuszel.domain.Product
+import com.dylanmuszel.melichallenge.framework.core.network.ConnectivityInfo
+import com.dylanmuszel.melichallenge.framework.core.network.NetworkResponse
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import retrofit2.HttpException
+
+class ProductNetworkDataSourceTest {
+
+    lateinit var connectivityInfo: ConnectivityInfo
+    lateinit var productService: ProductService
+    lateinit var productNetworkDataSource: ProductNetworkDataSource
+
+    @Before
+    fun setup() {
+        connectivityInfo = mock()
+        productService = mock()
+        productNetworkDataSource = ProductNetworkDataSource(connectivityInfo, productService)
+    }
+
+    @Test
+    fun `given offline connectivity when searching then returns a network failure`() = runBlocking {
+
+        // GIVEN
+        whenever(connectivityInfo.isOnline).thenReturn(false)
+
+        // WHEN
+        val result = productNetworkDataSource.search("query")
+
+        // THEN
+        assertThat(result.isLeft(), `is`(true))
+        result.fold({ assertThat(it, instanceOf(NetworkConnectionFailure::class.java)) }, {})
+    }
+
+    @Test
+    fun `given online connectivity but http exception when searching then returns a server failure`() = runBlocking {
+
+        // GIVEN
+        val exception = mock<HttpException>()
+        whenever(connectivityInfo.isOnline).thenReturn(true)
+        whenever(productService.search(any())).thenThrow(exception)
+
+        // WHEN
+        val result = productNetworkDataSource.search("query")
+
+        // THEN
+        assertThat(result.isLeft(), `is`(true))
+        result.fold({
+            assertThat(it, instanceOf(ServerFailure::class.java))
+            assertThat((it as ServerFailure).exception, instanceOf(HttpException::class.java))
+        }, {})
+    }
+
+    @Test
+    fun `given a list of products when searching then returns the product list`() = runBlocking {
+
+        // GIVEN
+        val list = listOf<Product>(mock(), mock())
+        whenever(connectivityInfo.isOnline).thenReturn(true)
+        whenever(productService.search(any())).thenReturn(NetworkResponse(list))
+
+        // WHEN
+        val result = productNetworkDataSource.search("query")
+
+        // THEN
+        assertThat(result.isRight(), `is`(true))
+        assertThat(result.orNull(), `is`(list))
+    }
+}
